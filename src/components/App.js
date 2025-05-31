071fbf3eca8bee310b8d088c6ca8d40fe6761477
Hitesh Rathi <97667730+HiteshRathi02@users.noreply.github.com>
2025-05-31 17:05:32 +0000

    Render dynamic component

--- src/components/App.js
+++ src/components/App.js
@@ -2,8 +2,30 @@ import React, {Component, useState} from "react";
 import '../styles/App.css';
 
 const App = () => {
+
+  const projects = [
+  {
+    name: "AI Personal Assistant",
+    description: "An assistant that automates daily tasks using AI.",
+  },
+  {
+    name: "Smart Farming System",
+    description: "A system that monitors soil and weather for efficient farming.",
+  }
+];
+
+
   return (
-    <div id="main"></div>
+    <div id="ns-main" className="ns-wrapper">
+      {projects.map((item,index)=>{
+        return (
+          < div key={index}>
+          <h1 data-ns-test= "project-name" >{item.name}</h1>
+          <h6 data-ns-test= "project-description">{item.description}</h6>
+          </div>
+        )
+      })}
+    </div>
   )
 }
 
